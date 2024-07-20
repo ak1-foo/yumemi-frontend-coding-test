ac8be1ddec51032932b40e74a672fa1daeb48dde
ak1-foo <74660186+ak1-foo@users.noreply.github.com>
2024-07-20 11:44:11 +0000

    feat: add atom checkbox component

--- src/app.vue
+++ src/app.vue
@@ -1,6 +1,27 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <p>
+      <AtomsAtomCheckbox v-model="checked" />
+      checked = {{ checked }}
+    </p>
+    <p>
+      <AtomsAtomCheckbox
+        v-model="checkedArray"
+        value="foo"
+      />
+      foo
+      <AtomsAtomCheckbox
+        v-model="checkedArray"
+        value="bar"
+      />
+      bar
+      <br />
+      checkedArray = {{ checkedArray }}
+    </p>
   </div>
 </template>
+
+<script setup lang="ts">
+const checked = ref<boolean>(false);
+const checkedArray = ref<string[]>([]);
+</script>
---
+++ src/components/atoms/AtomCheckbox.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <input
+      v-model="checked"
+      :value="prop.value"
+      type="checkbox"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+const checked = defineModel<string[] | boolean>();
+const prop = defineProps<{
+  value?: string;
+}>();
+</script>
