2e123dbd7fcfacf3e0570db0d11ed6bef6c5cccb
ak1-foo <74660186+ak1-foo@users.noreply.github.com>
2024-07-20 11:50:24 +0000

    feat: add organism select prefecture component

--- src/app.vue
+++ src/app.vue
@@ -1,17 +1,5 @@
 <template>
   <div>
-    <ModulesModuleCheckboxLabel
-      v-model="checkedArray"
-      label="label1"
-    />
-    <ModulesModuleCheckboxLabel
-      v-model="checkedArray"
-      label="label2"
-    />
-    checkedArray: {{ checkedArray }}
+    <OrganismsOrganismSelectPrefecture />
   </div>
 </template>
-
-<script setup lang="ts">
-const checkedArray = ref<string[]>([]);
-</script>
