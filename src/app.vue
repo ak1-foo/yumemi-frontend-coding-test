5daf6292dbb8c4da41b5999e87d169b766e636d3
ak1-foo <74660186+ak1-foo@users.noreply.github.com>
2024-07-20 11:47:34 +0000

    feat: add module checkbox and label component

--- src/app.vue
+++ src/app.vue
@@ -1,27 +1,17 @@
 <template>
   <div>
-    <p>
-      <AtomsAtomCheckbox v-model="checked" />
-      checked = {{ checked }}
-    </p>
-    <p>
-      <AtomsAtomCheckbox
-        v-model="checkedArray"
-        value="foo"
-      />
-      foo
-      <AtomsAtomCheckbox
-        v-model="checkedArray"
-        value="bar"
-      />
-      bar
-      <br />
-      checkedArray = {{ checkedArray }}
-    </p>
+    <ModulesModuleCheckboxLabel
+      v-model="checkedArray"
+      label="label1"
+    />
+    <ModulesModuleCheckboxLabel
+      v-model="checkedArray"
+      label="label2"
+    />
+    checkedArray: {{ checkedArray }}
   </div>
 </template>
 
 <script setup lang="ts">
-const checked = ref<boolean>(false);
 const checkedArray = ref<string[]>([]);
 </script>
